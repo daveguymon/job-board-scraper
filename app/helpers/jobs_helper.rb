a884add57d6f1594234edab3ba4f99f2b1ad1aa0
Dave Guymon <davidwguymon@gmail.com>
2020-08-29 19:13:57 -0600

    Basic UI styling with bootstrap.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,14 @@
 module JobsHelper
+  def parseLinks(description)
+    url_regexp = %r{
+      (?:(?:https?|ftp|file):\/\/|www\.|ftp\.)
+      (?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|
+           [-A-Z0-9+&@#\/%=~_|$?!:,.])*
+      (?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|
+            [A-Z0-9+&@#\/%=~_|$])
+    }ix
+
+    description.gsub(/(Watch)/, " Watch")
+    description.gsub(url_regexp, '<a href="\0">\0</a>').html_safe
+  end
 end
